75b71d340edd0d1e6d0e290729e34adcc87765ed
Tsoy Yuriy <teddyrain79@gmail.com>
2024-04-15 21:31:20 +0500

    добабвил контейнер

--- src/App.js
+++ src/App.js
@@ -1,62 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import Animals from "./components/Animals/Animals";
-import { AddFormAnimals } from "./components/AddFormAnimals/AddFormAnimals";
+import { AnimalsContainer } from "./containers/AnimalsContainer/AnimalsContainer";
 
-const getRandomKey = () =>
-  Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
 
 function App() {
-  const [animals, setAnimals] = useState([
-    {
-      id: getRandomKey(),
-      nameAnimal: "Smoky",
-      age: 8,
-      type: "cat",
-      img: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-
-    {
-      id: getRandomKey(),
-      nameAnimal: "Baksi",
-      age: 4,
-      type: "dog",
-      img: "https://images.pexels.com/photos/18745826/pexels-photo-18745826/free-photo-of-small-white-dog-in-a-camo-vest-sitting-in-front-of-a-bowl-on-the-grass.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-  ]);
-
-  const [item, setItem] = useState({
-    id: getRandomKey(),
-    nameAnimal: "",
-    age: "",
-    type: "",
-    img: "",
-  });
-
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setItem((prev) => {
-      return { ...prev, [name]: value };
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setAnimals([item, ...animals]);
-  };
-
-  const deleteCard = (id) => {
-    const copyAnimals = animals.filter((animal) => animal.id !== id);
-    setAnimals(copyAnimals);
-  };
 
   return (
     <div className="App">
-      <AddFormAnimals handleSubmit={handleSubmit} handleInput={handleInput} />
-
-      <Animals animals={animals} deleteCard={deleteCard} />
+      <AnimalsContainer/>
     </div>
   );
 }
---
+++ src/containers/AnimalsContainer/AnimalsContainer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { AddFormAnimals } from "../../components/AddFormAnimals/AddFormAnimals";
+import Animals from "../../components/Animals/Animals";
+import { useState } from "react";
+
+const getRandomKey = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+    
+export const AnimalsContainer = () => {
+
+  const [animals, setAnimals] = useState([
+    {
+      id: getRandomKey(),
+      nameAnimal: "Smoky",
+      age: 8,
+      type: "cat",
+      img: "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+
+    {
+      id: getRandomKey(),
+      nameAnimal: "Baksi",
+      age: 4,
+      type: "dog",
+      img: "https://images.pexels.com/photos/18745826/pexels-photo-18745826/free-photo-of-small-white-dog-in-a-camo-vest-sitting-in-front-of-a-bowl-on-the-grass.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+  ]);
+
+  const [item, setItem] = useState({
+    id: getRandomKey(),
+    nameAnimal: "",
+    age: "",
+    type: "",
+    img: "",
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setItem((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAnimals([item, ...animals]);
+  };
+
+  const deleteCard = (id) => {
+    const copyAnimals = animals.filter((animal) => animal.id !== id);
+    setAnimals(copyAnimals);
+  };
+  return (
+    <div>
+      <AddFormAnimals handleSubmit={handleSubmit} handleInput={handleInput} />
+
+      <Animals animals={animals} deleteCard={deleteCard} />
+    </div>
+  );
+};
